4d4bfb5dfb7c71a0ec53918298ace47d579ac4e7
Francisco Borja <fborja1414@gmail.com>
2021-06-13 00:51:10 -0700

    set grid position for each project, cursor ux nav

--- nuxt/components/Section.vue
+++ nuxt/components/Section.vue
@@ -1,7 +1,18 @@
 <template>
   <div class="section-container" :class="position">
-    <div class="items" v-for="(items, match) in entry" :key="match">
-      {{ items }} : {{ match }}
+    <div class="scroll" v-if="images.length > 1">
+      <img
+        v-for="(image, index) in images"
+        :key="index"
+        :src="
+          image.url.split('/uploads/').join('http://localhost:1337/uploads/')
+        "
+      />
+    </div>
+    <div class="description">
+      <a class="title">2021 {{ entry.name }}</a>
+      <a class="title">{{ description }}</a>
+      <a class="swipe" v-if="images.length > 1">swipe -> </a>
     </div>
   </div>
 </template>
@@ -15,19 +26,43 @@ export default {
   },
 
   computed: {
+    images: function () {
+      return this.entry.images;
+    },
     position: function () {
       return this.entry.position;
+    },
+    description: function () {
+      return this.entry.description;
     },
   },
 };
 </script>
 
 <style lang="scss" scoped>
-.section-container {
-  grid-column: 7/13;
+@import "~assets/_projects.scss";
+
+.title {
+  font-style: italic;
+  height: 5rem;
 }
 
-.items {
-  display: block;
+.scroll {
+  display: flex;
+  overflow-x: scroll;
+  scroll-snap-type: x mandatory;
+}
+
+.description {
+  display: flex;
+  justify-content: space-between;
+}
+img {
+  width: 100%;
+  margin: 0 0 1rem 0;
+}
+
+.swipe {
+  text-align: right;
 }
 </style>
--- nuxt/pages/index.vue
+++ nuxt/pages/index.vue
@@ -7,11 +7,17 @@
       </a>
     </div>
     <div class="navigation">
-      <a>> Resume </a>
-      <a>> Elliot Evan</a>
-      <a>> Grace Bukunmi </a>
-      <a>> Fast Magazine 2020 Pages </a>
-      <a>> Graphics </a>
+      <div
+        class="nav-container"
+        v-for="(entries, index) in projects"
+        :key="index"
+      >
+        <div class="nav-items" :key="index">
+          <a :key="index" class="select"> > </a>
+          <a> {{ entries.name }}</a>
+        </div>
+        <a :key="index" class="medium"> {{ entries.medium }}</a>
+      </div>
     </div>
     <template v-for="(entry, index) in projects">
       <Section :entry="entry" :index="index" :key="index" />
@@ -24,6 +30,17 @@ import Section from "@/components/Section.vue";
 export default {
   components: {
     Section,
+  },
+  data() {
+    return {
+      navHovered: false,
+    };
+  },
+  methods: {
+    hoveredNav() {
+      this.navHovered = true;
+      console.log("ha");
+    },
   },
   async asyncData({ $axios }) {
     const projects = await $axios.$get("http://localhost:1337/projects");
@@ -52,13 +69,36 @@ export default {
 }
 
 .navigation {
+  position: relative;
   display: flex;
   grid-column: 5/11;
   font-size: 20px;
   flex-direction: column;
+  height: 55vh;
   margin: 0 0 6rem 0;
+  overflow-x: visible;
 }
-.items {
-  display: block;
+.medium {
+  font-style: italic;
+}
+
+.select {
+  position: absolute;
+  left: -2vw;
+  opacity: 0;
+}
+
+.nav-items {
+  position: relative;
+}
+
+.nav-container {
+  cursor: pointer;
+}
+
+.nav-container:hover {
+  .select {
+    opacity: 1;
+  }
 }
 </style>
